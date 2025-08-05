874800c0741132d7c5a63fa8a7254e7dc065d354
amaan8429 <mohdamaan8429@gmail.com>
2025-08-05 21:36:03 +0530

    refactor: update Tailwind CSS configuration and global styles; adjust color variables and font imports for improved design consistency and maintainability

--- cur8t-web/src/app/layout.tsx
+++ cur8t-web/src/app/layout.tsx
@@ -4,39 +4,29 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/layout/ThemeProvider";
 import QueryProvider from "@/components/providers/QueryProvider";
 import { ErrorBoundary } from "@/components/providers/ErrorBoundary";
-import {
-  Inter,
-  Poppins,
-  Libre_Baskerville,
-  IBM_Plex_Mono,
-} from "next/font/google";
+import { Oxanium, Merriweather, Fira_Code } from "next/font/google";
 
 export const metadata = {
   title: "Cur8t",
   description: "a way to share links faster and optmize your workflow",
 };
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const poppins = Poppins({
+const oxanium = Oxanium({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
-const libreBaskerville = Libre_Baskerville({
+const merriweather = Merriweather({
   variable: "--font-serif",
   subsets: ["latin"],
-  weight: ["400", "700"],
+  weight: ["300", "400", "700", "900"],
 });
 
-const ibmPlexMono = IBM_Plex_Mono({
+const firaCode = Fira_Code({
   variable: "--font-mono",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export default function RootLayout({
@@ -48,7 +38,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${inter.variable} ${poppins.variable} ${libreBaskerville.variable} ${ibmPlexMono.variable} theme font-sans`}
+          className={`${oxanium.variable} ${merriweather.variable} ${firaCode.variable} theme font-sans`}
         >
           <ThemeProvider attribute="class" defaultTheme="system">
             <QueryProvider>
